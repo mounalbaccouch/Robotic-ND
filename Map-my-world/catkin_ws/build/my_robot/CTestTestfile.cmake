28b8c6b99b83afd49e18dc133a8ba631b110bc5d
Mouna <malbaccouch@gmail.com>
2020-12-22 18:59:28 +0100

    Removing false commit: build directory should be created by catkin_make

--- Map-my-world/catkin_ws/build/my_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mouna/workspace/Robotic-ND/Map-my-world/catkin_ws/src/my_robot
-# Build directory: /home/mouna/workspace/Robotic-ND/Map-my-world/catkin_ws/build/my_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Map-my-world/catkin_ws/build/teleop_twist_keyboard/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mouna/workspace/Robotic-ND/Map-my-world/catkin_ws/src/teleop_twist_keyboard
-# Build directory: /home/mouna/workspace/Robotic-ND/Map-my-world/catkin_ws/build/teleop_twist_keyboard
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
